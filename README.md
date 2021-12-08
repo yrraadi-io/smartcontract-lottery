3482a807cefecc69cd3a95bcfa42e677ada1a70a
Yash Rathod <yrraadi@gmail.com>
2021-12-07 20:20:32 -0600

    brownie-config, test_lottery & Lottery.sol updated.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 1. Users can enter lottery with ETH based on USD fee.
 2. An admin will choose when lottery is over.
 3. The lottery will select a random winner.
+
+How do we test this?
+
+1. `mainnet-fork`
+2. `development` with mocks
+3. `testnet`
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,3 +1,64 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {}
+contract Lottery is Ownable {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
+    // 0
+    // 1
+    // 2
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+    }
+
+    function enter() public payable {
+        // $50 min
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // 16 decimals
+        // $50, $2,000 / ETH
+        // 50/2,000
+        // 50 * 100000 / 2000
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        uint256(
+            keccak256(
+                abi.encodePacked(
+                    nonce,
+                    msg.sender,
+                    block.difficulty,
+                    block.timestamp
+                )
+            )
+        ) % players.length;
+    }
+}
